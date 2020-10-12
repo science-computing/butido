fd429744a36fc27b8a490bf1131225a01de5de77
Matthias Beyer <mail@beyermatthias.de>
2020-10-12 20:52:48 +0200

    Add dummy implementations for Executor and VersionParser
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/package/version.rs
+++ src/package/version.rs
@@ -10,3 +10,17 @@ pub trait NameVersionBuffer {
     fn get_as_str(&self) -> &str;
 }
 
+pub struct DummyVersionParser;
+impl DummyVersionParser {
+    pub fn new() -> Self {
+        DummyVersionParser
+    }
+}
+
+impl VersionParser for DummyVersionParser {
+    fn parse(&self, buffer: &dyn NameVersionBuffer) -> Result<(PackageName, PackageVersionConstraint)> {
+        let v: Vec<_> = buffer.get_as_str().split("-").collect();
+        Ok((PackageName::from(String::from(v[0])), PackageVersionConstraint::Any))
+    }
+}
+
--- src/util/executor.rs
+++ src/util/executor.rs
@@ -7,3 +7,16 @@ pub trait Executor {
     fn execute_dependency_script(&self, scriptpath: &Path) -> Result<Vec<Dependency>>;
 }
 
+pub struct DummyExecutor;
+impl DummyExecutor {
+    pub fn new() -> Self {
+        DummyExecutor
+    }
+}
+
+impl Executor for DummyExecutor {
+    fn execute_dependency_script(&self, _scriptpath: &Path) -> Result<Vec<Dependency>> {
+        Ok(vec![])
+    }
+}
+
