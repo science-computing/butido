c58c6ea8cd054a8606a91fe48cd4d1b3f6052403
Matthias Beyer <mail@beyermatthias.de>
2020-10-14 18:53:02 +0200

    Add more configuration for docker endpoints
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -44,8 +44,34 @@ impl Deref for Configuration {
 }
 
 
-#[derive(Debug, Getters, Deserialize)]
+#[derive(Debug, Getters, CopyGetters, Deserialize)]
 pub struct DockerConfig {
+    /// The required docker version
+    ///
+    /// If not set, it will not be checked, which might result in weird things?
+    ///
+    /// # Note
+    ///
+    /// Because the docker API returns strings, not a version object, each compatible version must
+    /// be listed.
+    #[getset(get = "pub")]
+    docker_versions: Option<Vec<String>>,
+
+    /// The required docker api version
+    ///
+    /// If not set, it will not be checked, which might result in weird things?
+    ///
+    /// # Note
+    ///
+    /// Because the docker API returns strings, not a version object, each compatible version must
+    /// be listed.
+    #[getset(get = "pub")]
+    docker_api_versions: Option<Vec<String>>,
+
+    /// Whether the program should verify that the required images are present.
+    /// You want this to be true normally.
+    #[getset(get_copy = "pub")]
+    verify_images_present: bool,
 
     #[getset(get = "pub")]
     images: Vec<ImageName>,
