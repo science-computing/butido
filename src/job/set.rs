science-computing/butido

commit 97662a1abb69f7ddc060dbf70409478d21c21e0b

Move progress bar setup to utility module
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/tree.rs
@@ -19,7 +19,7 @@ impl Tree {
         Tree { root: BTreeMap::new() }
     }
 
-    pub fn add_package(&mut self, p: Package, repo: &Repository, executor: &dyn Executor, progress: &ProgressBar) -> Result<()> {
+    pub fn add_package(&mut self, p: Package, repo: &Repository, executor: &dyn Executor, progress: ProgressBar) -> Result<()> {
         macro_rules! mk_add_package_tree {
             ($this:ident, $pack:ident, $repo:ident, $root:ident, $executor:ident, $progress:ident) => {{
                 let mut subtree = Tree::new();
@@ -42,7 +42,7 @@ impl Tree {
                             .map(|p| {
                                 ($progress).tick();
                                 trace!("Following dependecy: {:?}", p);
-                                add_package_tree(&mut subtree, p.clone(), ($repo), ($root), ($executor), ($progress))
+                                add_package_tree(&mut subtree, p.clone(), ($repo), ($root), ($executor), ($progress).clone())
                             })
                             .collect()
                     })
@@ -54,7 +54,7 @@ impl Tree {
             }}
         };
 
-        fn add_package_tree(this: &mut Tree, p: Package, repo: &Repository, root: &mut Tree, executor: &dyn Executor, progress: &ProgressBar) -> Result<()> {
+        fn add_package_tree(this: &mut Tree, p: Package, repo: &Repository, root: &mut Tree, executor: &dyn Executor, progress: ProgressBar) -> Result<()> {
             mk_add_package_tree!(this, p, repo, root, executor, progress)
         }
 

src/main.rs
@@ -27,6 +27,7 @@ use crate::util::executor::DummyExecutor;
 use crate::package::Tree;
 use crate::filestore::ReleaseStore;
 use crate::filestore::StagingStore;
+use crate::util::progress::ProgressBars;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -47,7 +48,7 @@ async fn main() -> Result<()> {
     let config: Configuration = config.try_into::<NotValidatedConfiguration>()?.validate()?;
     let repo_path    = PathBuf::from(config.repository());
     let max_packages = count_pkg_files(&repo_path, ProgressBar::new_spinner());
-    let progressbars = setup_progressbars(max_packages);
+    let progressbars = ProgressBars::setup(max_packages);
 
     let release_dir  = async {
         let variables = BTreeMap::new();
@@ -90,10 +91,8 @@ async fn main() -> Result<()> {
 
     let trees = tokio::stream::iter(packages.into_iter().cloned())
         .map(|p| {
-            let bar = progressbars.root.add(tree_building_progress_bar(max_packages));
-            bar.set_message(&format!("Building Package Tree for {}", p.name()));
             let mut tree = Tree::new();
-            tree.add_package(p, &repo, &DummyExecutor::new(), &bar)?;
+            tree.add_package(p, &repo, &DummyExecutor::new(), progressbars.tree_building.clone())?;
             Ok(tree)
         })
         .collect::<Result<Vec<_>>>()
@@ -106,42 +105,6 @@ async fn main() -> Result<()> {
     }
 
     progressbars.root.join().map_err(Error::from)
-}
-
-struct ProgressBars {
-    root:            MultiProgress,
-    release_loading: ProgressBar,
-    staging_loading: ProgressBar,
-    repo_loading:    ProgressBar,
-}
-
-fn setup_progressbars(max_packages: u64) -> ProgressBars {
-    fn bar(msg: &str, max_packages: u64) -> ProgressBar {
-        let b = ProgressBar::new(max_packages);
-        b.set_style({
-            ProgressStyle::default_bar()
-                .template("[{elapsed_precise}] {pos:>3}/{len:>3} ({percent:>3}%): {bar} | {msg}")
-        });
-
-        b.set_message(msg);
-        b
-    }
-
-    let root = MultiProgress::new();
-    ProgressBars {
-        repo_loading:    root.add(bar("Repository loading", max_packages)),
-        staging_loading: root.add(bar("Loading staging", max_packages)),
-        release_loading: root.add(bar("Loading releases", max_packages)),
-        root,
-    }
-}
-
-fn tree_building_progress_bar(max: u64) -> ProgressBar {
-    let b = ProgressBar::new(max);
-    b.set_style({
-        ProgressStyle::default_bar().template("[{elapsed_precise}] {pos:>3}/{len:>3} ({percent:>3}%): {bar} | {msg}")
-    });
-    b
 }
 
 fn count_pkg_files(p: &Path, progress: ProgressBar) -> u64 {

src/util/mod.rs
@@ -5,6 +5,7 @@ pub struct EnvironmentVariableName(String);
 
 pub mod executor;
 pub mod parser;
+pub mod progress;
 
 pub mod docker {
     use anyhow::Result;

src/util/progress.rs
@@ -1,0 +1,35 @@
+use indicatif::*;
+
+pub struct ProgressBars {
+    pub root:            MultiProgress,
+    pub release_loading: ProgressBar,
+    pub staging_loading: ProgressBar,
+    pub repo_loading:    ProgressBar,
+    pub tree_building:   ProgressBar,
+}
+
+impl ProgressBars {
+    pub fn setup(max_packages: u64) -> Self {
+        fn bar(msg: &str, max_packages: u64) -> ProgressBar {
+            let b = ProgressBar::new(max_packages);
+            b.set_style({
+                ProgressStyle::default_bar()
+                    .template("[{elapsed_precise}] {pos:>3}/{len:>3} ({percent:>3}%): {bar} | {msg}")
+            });
+
+            b.set_message(msg);
+            b
+        }
+
+        let root = MultiProgress::new();
+        ProgressBars {
+            repo_loading:    root.add(bar("Repository loading", max_packages)),
+            staging_loading: root.add(bar("Loading staging", max_packages)),
+            release_loading: root.add(bar("Loading releases", max_packages)),
+            tree_building:   root.add(bar("Building package tree", max_packages)),
+            root,
+        }
+    }
+}
+
+

commit 14b17b35ad74c1f915ec7ebe31b242e3a8cd87dd

Fix (tests): Do not pass progress bars by reference
Fixes: 97662a1abb ("Move progress bar setup to utility module")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/job/set.rs
@@ -113,7 +113,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
 
         let image  = ImageName::from(String::from("test"));
@@ -159,10 +159,10 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
 
-        let r = tree.add_package(p2, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p2, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
 
         let image  = ImageName::from(String::from("test"));
@@ -216,7 +216,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
 
         let image  = ImageName::from(String::from("test"));

src/package/tree.rs
@@ -170,7 +170,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
     }
 
@@ -199,10 +199,10 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
 
-        let r = tree.add_package(p2, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p2, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
     }
 
@@ -237,7 +237,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
         assert!(tree.packages().all(|p| *p.name() == pname("a")));
         assert!(tree.packages().all(|p| *p.version() == pversion("1")));
@@ -330,7 +330,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
         assert!(tree.packages().all(|p| *p.name() == pname("p1")));
         assert!(tree.packages().all(|p| *p.version() == pversion("1")));
@@ -503,7 +503,7 @@ mod tests {
         let progress = ProgressBar::hidden();
 
         let mut tree = Tree::new();
-        let r = tree.add_package(p1, &repo, &dummy_executor, &progress);
+        let r = tree.add_package(p1, &repo, &dummy_executor, progress.clone());
         assert!(r.is_ok());
         assert!(tree.packages().all(|p| *p.name() == pname("p1")));
         assert!(tree.packages().all(|p| *p.version() == pversion("1")));