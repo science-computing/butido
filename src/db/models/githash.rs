d21c8858e653cbfbf779a103110c7d0342e21114
Matthias Beyer <mail@beyermatthias.de>
2020-11-05 11:47:45 +0100

    Add NewGitHash for inserting git hashes into database
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/db/models/githash.rs
+++ src/db/models/githash.rs
@@ -1,6 +1,14 @@
+use crate::schema::githashes;
+
 #[derive(Queryable)]
 pub struct GitHash {
     pub id: i32,
     pub hash: String,
+}
+
+#[derive(Insertable)]
+#[table_name="githashes"]
+pub struct NewGitHash<'a> {
+    pub hash: &'a str,
 }
 
