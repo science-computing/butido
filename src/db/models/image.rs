bf9e86db80e9b66e1c67160a8b46aa60cb205692
Matthias Beyer <mail@beyermatthias.de>
2020-11-05 11:51:59 +0100

    Add NewImage type for inserting image into database
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/db/models/image.rs
+++ src/db/models/image.rs
@@ -1,5 +1,14 @@
+use crate::schema::images;
+
 #[derive(Queryable)]
 pub struct Image {
     pub id: i32,
     pub name: String,
 }
+
+#[derive(Insertable)]
+#[table_name="images"]
+pub struct NewImage<'a> {
+    pub name: &'a str,
+}
+
