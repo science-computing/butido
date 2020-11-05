4189dfbec95e44b6afca285c0192721b24e68883
Matthias Beyer <mail@beyermatthias.de>
2020-11-05 11:35:35 +0100

    Add NewPackage type for adding a package to the database
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/db/models/package.rs
+++ src/db/models/package.rs
@@ -1,7 +1,16 @@
+use crate::schema::packages;
+
 #[derive(Queryable)]
 pub struct Package {
     pub id: i32,
     pub name: String,
     pub version: String,
+}
+
+#[derive(Insertable)]
+#[table_name="packages"]
+pub struct NewPackage<'a> {
+    pub name: &'a str,
+    pub version: &'a str,
 }
 
