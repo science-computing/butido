b3b2e36350123913044e26f374c0825c257125f4
Matthias Beyer <mail@beyermatthias.de>
2020-11-05 10:01:30 +0100

    Add helper to check whether repository is clean
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/util/git.rs
+++ src/util/git.rs
@@ -4,6 +4,12 @@ use anyhow::Result;
 use anyhow::Error;
 use git2::Repository;
 
+pub fn repo_is_clean(p: &Path) -> Result<bool> {
+    Repository::open(p)
+        .map_err(Error::from)
+        .map(|r| r.state() == git2::RepositoryState::Clean)
+}
+
 pub fn get_repo_head_commit_hash(p: &Path) -> Result<String> {
     let r = Repository::open(p)?;
     let hash = r.head()?
