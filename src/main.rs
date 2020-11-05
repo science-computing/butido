d6a4f1d4dbc8d5443ed82c3e5873c15f5d5e5bb2
Matthias Beyer <mail@beyermatthias.de>
2020-11-05 10:06:22 +0100

    Add repository cleanness check in UI
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,7 @@ async fn main() -> Result<()> {
 
     let config: Configuration = config.try_into::<NotValidatedConfiguration>()?.validate()?;
     let repo_path             = PathBuf::from(config.repository());
+    let _                     = crate::ui::package_repo_cleanness_check(&repo_path)?;
     let max_packages          = count_pkg_files(&repo_path, ProgressBar::new_spinner());
     let mut progressbars      = ProgressBars::setup();
 
