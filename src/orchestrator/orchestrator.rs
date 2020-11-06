8dc44d0c66236ce8a0cf9fc2ea90b53f1d702bfb
Matthias Beyer <mail@beyermatthias.de>
2020-11-06 09:53:04 +0100

    Implement orchestration routine
    This patch adds the first implementation of the orchestration routine.
    
    This implementation does _NOT_ yet include any database stuff or any
    output manouvering, such as progress reporting to CLI or log routing.
    It is just the bare minimal to get a working POC.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/orchestrator/orchestrator.rs
+++ src/orchestrator/orchestrator.rs
@@ -1,10 +1,17 @@
 use std::path::PathBuf;
 use std::sync::RwLock;
 use std::sync::Arc;
+use std::collections::BTreeMap;
 
+use anyhow::anyhow;
+use anyhow::Error;
+use anyhow::Context;
 use anyhow::Result;
 use typed_builder::TypedBuilder;
 use diesel::PgConnection;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc::UnboundedSender;
+use resiter::AndThen;
 
 use crate::endpoint::EndpointConfiguration;
 use crate::endpoint::EndpointScheduler;
@@ -15,6 +22,10 @@ use crate::filestore::StagingStore;
 use crate::filestore::ReleaseStore;
 use crate::log::FileLogSinkFactory;
 use crate::log::LogSink;
+use crate::db::models::Submit;
+use crate::db::models::EnvVar;
+use crate::job::JobResource;
+use crate::filestore::MergedStores;
 
 pub struct Orchestrator {
     scheduler: EndpointScheduler,
@@ -32,6 +43,7 @@ pub struct OrchestratorSetup {
     release_store: Arc<RwLock<ReleaseStore>>,
     jobsets: Vec<JobSet>,
     database: PgConnection,
+    submit: Submit,
     file_log_sink_factory: Option<FileLogSinkFactory>,
 }
 
@@ -53,7 +65,56 @@ impl OrchestratorSetup {
 impl Orchestrator {
 
     pub async fn run(self) -> Result<()> {
-        unimplemented!()
+        use tokio::stream::StreamExt;
+
+        let _database = self.database;
+        for jobset in self.jobsets.into_iter() {
+            let merged_store = MergedStores::new(self.release_store.clone(), self.staging_store.clone());
+
+            let results = { // run the jobs in the set
+                let unordered = futures::stream::FuturesUnordered::new();
+                for runnable in jobset.into_runables(&merged_store) {
+                    let (sender, receiver) = tokio::sync::mpsc::unbounded_channel::<LogItem>();
+
+                    let jobhandle = self.scheduler.schedule_job(runnable?, sender).await?;
+                    unordered.push(async move {
+                        jobhandle.get_result().await
+                    });
+                }
+
+                unordered.collect::<Result<Vec<_>>>().await?
+                    .into_iter()
+                    .flatten()
+                    .collect::<Vec<PathBuf>>()
+            };
+
+            { // check if all paths that were written are actually there in the staging store
+                let staging_store_lock = self.staging_store
+                    .read()
+                    .map_err(|_| anyhow!("Lock Poisoned"))?;
+
+                for path in results.iter() {
+                    if !staging_store_lock.path_exists_in_store_root(&path) {
+                        return Err(anyhow!("Result path {} is missing from staging store", path.display()))
+                            .with_context(|| anyhow!("Should be: {}/{}", staging_store_lock.root_path().display(), path.display()))
+                            .map_err(Error::from)
+                    }
+                }
+            }
+
+            { // register all written paths to the store
+                let mut staging_store_lock = self.staging_store
+                    .write()
+                    .map_err(|_| anyhow!("Lock Poisoned"))?;
+
+                for path in results.iter() {
+                    staging_store_lock.load_from_path(&path)
+                        .context("Loading artifacts into staging store")?;
+                }
+            }
+        }
+
+        Ok(())
     }
 
 }
