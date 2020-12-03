science-computing/butido

commit 788c81a44561942841a261337366e4a6e1059386

Add ability to use variables from the package in its build script using handlebars syntax
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/script.rs
@@ -1,4 +1,6 @@
+use anyhow::Error;
 use anyhow::Result;
+use handlebars::Handlebars;
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -69,6 +71,12 @@ impl<'a> ScriptBuilder<'a> {
             }
         }
 
-        Ok(Script(script))
+        Self::interpolate_package(script, package).map(Script)
+    }
+
+    fn interpolate_package(script: String, package: &Package) -> Result<String> {
+        let mut hb = Handlebars::new();
+        hb.register_template_string("script", script)?;
+        hb.render("releases", package).map_err(Error::from)
     }
 }

commit bc61eb15068ebc34e60a5dee144d0912794dc3e1

Fix: Template name
Fixes: 788c81a4 ("Add ability to use variables from the package in its build script using handlebars syntax")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/script.rs
@@ -77,6 +77,6 @@ impl<'a> ScriptBuilder<'a> {
     fn interpolate_package(script: String, package: &Package) -> Result<String> {
         let mut hb = Handlebars::new();
         hb.register_template_string("script", script)?;
-        hb.render("releases", package).map_err(Error::from)
+        hb.render("script", package).map_err(Error::from)
     }
 }