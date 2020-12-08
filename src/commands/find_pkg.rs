a21e30502c7cd9e06aaa2129c24e3198ba9065ed
Matthias Beyer <mail@beyermatthias.de>
2020-12-08 09:43:04 +0100

    Rewrite find-pkg filtering to support optional package version constraints
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/commands/find_pkg.rs
+++ src/commands/find_pkg.rs
@@ -4,31 +4,31 @@ use clap::ArgMatches;
 use log::trace;
 
 use crate::config::Configuration;
-use crate::package::Package;
+use crate::package::PackageVersionConstraint;
 use crate::repository::Repository;
 
 pub async fn find_pkg(matches: &ArgMatches, config: &Configuration, repo: Repository) -> Result<()> {
-    use filters::filter::Filter;
     use std::io::Write;
 
-    let package_filter = {
-        let regex = matches
-            .value_of("package_name_regex")
-            .map(regex::RegexBuilder::new)
-            .map(|mut builder| {
-                builder.size_limit(1 * 1024 * 1024); // max size for the regex is 1MB. Should be enough for everyone
-                builder.build()
-                    .map_err(Error::from)
-            })
-            .unwrap()?; // safe by clap
+    let package_name_regex = matches
+        .value_of("package_name_regex")
+        .map(regex::RegexBuilder::new)
+        .map(|mut builder| {
+            builder.size_limit(1 * 1024 * 1024); // max size for the regex is 1MB. Should be enough for everyone
+            builder.build()
+                .map_err(Error::from)
+        })
+        .unwrap()?; // safe by clap
 
-        move |p: &Package| -> bool {
-            regex.captures(p.name()).is_some()
-        }
-    };
+    let package_version_constraint = matches
+        .value_of("package_version_constraint")
+        .map(String::from)
+        .map(PackageVersionConstraint::new)
+        .transpose()?;
 
     let iter = repo.packages()
-        .filter(|package| package_filter.filter(package))
+        .filter(|p| package_name_regex.captures(p.name()).is_some())
+        .filter(|p| package_version_constraint.as_ref().map(|v| v.matches(p.version())).unwrap_or(true))
         .inspect(|pkg| trace!("Found package: {:?}", pkg));
 
     let out = std::io::stdout();
