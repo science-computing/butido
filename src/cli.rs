ce2ef431eea7946c522b6660f50020ef511fc27b
Matthias Beyer <mail@beyermatthias.de>
2020-12-09 16:58:13 +0100

    Move script output modification argument construction into helper functions
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/cli.rs
+++ src/cli.rs
@@ -483,38 +483,10 @@ pub fn cli<'a>() -> App<'a> {
                 .short('s')
                 .about("Show the script of the package")
             )
-
-            .arg(Arg::new("script_line_numbers")
-                .required(false)
-                .multiple(false)
-                .long("line-numbers")
-                .about("Print script with line numbers (default)")
-                .conflicts_with("no_script_line_numbers")
-            )
-            .arg(Arg::new("no_script_line_numbers")
-                .required(false)
-                .multiple(false)
-                .long("no-line-numbers")
-                .about("Print script without line numbers")
-                .conflicts_with("script_line_numbers")
-            )
-
-            .arg(Arg::new("script_highlight")
-                .required(false)
-                .multiple(false)
-                .long("highlighting")
-                .alias("highlight")
-                .about("Print script with highlighting (default)")
-                .conflicts_with("no_script_highlight")
-            )
-            .arg(Arg::new("no_script_highlight")
-                .required(false)
-                .multiple(false)
-                .long("no-highlighting")
-                .alias("no-highlight")
-                .about("Print script without highlighting")
-                .conflicts_with("script_highlight")
-            )
+            .arg(script_arg_line_numbers())
+            .arg(script_arg_no_line_numbers())
+            .arg(script_arg_highlight())
+            .arg(script_arg_no_highlight())
 
         )
         .subcommand(App::new("source")
@@ -612,6 +584,44 @@ pub fn cli<'a>() -> App<'a> {
             )
         )
 
+}
+
+fn script_arg_line_numbers<'a>() -> clap::Arg<'a> {
+    Arg::new("script_line_numbers")
+        .required(false)
+        .multiple(false)
+        .long("line-numbers")
+        .about("Print script with line numbers (default)")
+        .conflicts_with("no_script_line_numbers")
+}
+
+fn script_arg_no_line_numbers<'a>() -> clap::Arg<'a> {
+    Arg::new("no_script_line_numbers")
+        .required(false)
+        .multiple(false)
+        .long("no-line-numbers")
+        .about("Print script without line numbers")
+        .conflicts_with("script_line_numbers")
+}
+
+fn script_arg_highlight<'a>() -> clap::Arg<'a> {
+    Arg::new("script_highlight")
+        .required(false)
+        .multiple(false)
+        .long("highlighting")
+        .alias("highlight")
+        .about("Print script with highlighting (default)")
+        .conflicts_with("no_script_highlight")
+}
+
+fn script_arg_no_highlight<'a>() -> clap::Arg<'a> {
+    Arg::new("no_script_highlight")
+        .required(false)
+        .multiple(false)
+        .long("no-highlighting")
+        .alias("no-highlight")
+        .about("Print script without highlighting")
+        .conflicts_with("script_highlight")
 }
 
 /// Naive check whether 's' is a 'key=value' pair or an existing environment variable
