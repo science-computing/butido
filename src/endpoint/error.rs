26cee7066649348546599249a1c1a103d130081b
Matthias Beyer <mail@beyermatthias.de>
2020-12-10 11:50:27 +0100

    Add printing of script error message
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/endpoint/error.rs
+++ src/endpoint/error.rs
@@ -4,16 +4,17 @@ use crate::util::docker::ContainerHash;
 
 #[derive(ThisError, Debug)]
 pub enum ContainerError {
-    #[error("Error during container run, connect using `docker --host {uri} exec -it {container_id} /bin/bash`")]
+    #[error("Error during container run:\n\tMessage: '{msg}'\n\tConnect using `docker --host {uri} exec -it {container_id} /bin/bash`")]
     ContainerError {
         container_id: ContainerHash,
         uri: String,
+        msg: String,
     },
 }
 
 impl ContainerError {
-    pub fn container_error(container_id: ContainerHash, uri: String) -> Self {
-        ContainerError::ContainerError { container_id, uri }
+    pub fn container_error(container_id: ContainerHash, uri: String, msg: String) -> Self {
+        ContainerError::ContainerError { container_id, uri, msg }
     }
 }
 
