science-computing/butido

commit 1d038bae2bc6c7215e5e9849fe418c6431e5adbd

Remove feature: State message in "OK" case.
The "OK" case had a state message parameter. This was removed because
"OK" does mean that everything went well. No need to have a message
here.

Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/script.rs
@@ -192,7 +192,6 @@ struct StateHelper;
 
 impl HelperDef for StateHelper {
     fn call<'reg: 'rc, 'rc>(&self, h: &Helper, _: &Handlebars, _: &Context, _rc: &mut RenderContext, out: &mut dyn Output) -> HelperResult {
-        let state_msg = h.param(1).ok_or_else(|| RenderError::new("Required parameter missing: state message"))?;
         h.param(0)
             .ok_or_else(|| RenderError::new("Required parameter missing: state"))?
             .value()
@@ -200,12 +199,11 @@ impl HelperDef for StateHelper {
             .ok_or_else(|| RenderError::new("Required parameter must be a string: state"))
             .and_then(|state| match state {
                 "OK" => {
-                    out.write("echo '#BUTIDO:STATE:OK:")?;
-                    out.write(state_msg.value().render().as_ref())?;
-                    out.write("'\n")?;
+                    out.write("echo '#BUTIDO:STATE:OK\n")?;
                     Ok(())
                 },
                 "ERR" => {
+                    let state_msg = h.param(1).ok_or_else(|| RenderError::new("Required parameter missing: state message"))?;
                     out.write("echo '#BUTIDO:STATE:ERR:")?;
                     out.write(state_msg.value().render().as_ref())?;
                     out.write("'\n")?;

src/commands/db.rs
@@ -402,7 +402,7 @@ fn job(conn_cfg: DbConnectionConfig, config: &Configuration, matches: &ArgMatche
                         LogItem::Line(s)         => Ok(String::from_utf8(s.to_vec())?.normal()),
                         LogItem::Progress(u)     => Ok(format!("#BUTIDO:PROGRESS:{}", u).bright_black()),
                         LogItem::CurrentPhase(p) => Ok(format!("#BUTIDO:PHASE:{}", p).bright_black()),
-                        LogItem::State(Ok(s))    => Ok(format!("#BUTIDO:STATE:OK:{}", s).green()),
+                        LogItem::State(Ok(()))   => Ok(format!("#BUTIDO:STATE:OK").green()),
                         LogItem::State(Err(s))   => Ok(format!("#BUTIDO:STATE:ERR:{}", s).red()),
                     })
                     .collect::<Result<Vec<_>>>()?

src/endpoint/scheduler.rs
@@ -239,9 +239,9 @@ impl<'a> LogReceiver<'a> {
                     trace!("Setting bar phase to {}", phasename);
                     self.bar.set_message(&format!("Job ({} {}): {} Phase: {}", self.package_name, self.package_version, self.job_id, phasename));
                 },
-                LogItem::State(Ok(ref s)) => {
-                    trace!("Setting bar state to Ok: {}", s);
-                    self.bar.set_message(&format!("Job ({} {}): {} State Ok: {}", self.package_name, self.package_version, self.job_id, s));
+                LogItem::State(Ok(())) => {
+                    trace!("Setting bar state to Ok");
+                    self.bar.set_message(&format!("Job ({} {}): {} State Ok", self.package_name, self.package_version, self.job_id));
                     success = Some(true);
                 },
                 LogItem::State(Err(ref e)) => {

src/log/item.rs
@@ -13,7 +13,7 @@ pub enum LogItem {
 
     /// The end-state of the process
     /// Either Ok or Error
-    State(Result<String, String>),
+    State(Result<(), String>),
 }
 
 impl LogItem {
@@ -22,7 +22,7 @@ impl LogItem {
             LogItem::Line(s)         => Ok(Display(String::from_utf8(s.to_vec())?)),
             LogItem::Progress(u)     => Ok(Display(format!("#BUTIDO:PROGRESS:{}", u))),
             LogItem::CurrentPhase(p) => Ok(Display(format!("#BUTIDO:PHASE:{}", p))),
-            LogItem::State(Ok(s))    => Ok(Display(format!("#BUTIDO:STATE:OK:{}", s))),
+            LogItem::State(Ok(()))   => Ok(Display(format!("#BUTIDO:STATE:OK"))),
             LogItem::State(Err(s))   => Ok(Display(format!("#BUTIDO:STATE:ERR:{}", s))),
         }
     }

src/log/parser.rs
@@ -83,7 +83,7 @@ pub fn parser<'a>() -> PomParser<'a, u8, LogItem> {
             (
                 (seq(b"STATE:ERR:") * string().map(|s| LogItem::State(Err(s))))
                 |
-                (seq(b"STATE:OK:") * string().map(|s| LogItem::State(Ok(s))))
+                seq(b"STATE:OK:").map(|_| LogItem::State(Ok(())))
             )
         )
     )
@@ -189,7 +189,7 @@ mod tests {
             Some other log line
             Some other log line
             Some other log line
-            #BUTIDO:STATE:OK:finished successfully
+            #BUTIDO:STATE:OK
         "};
 
         let p = parser();
@@ -249,7 +249,7 @@ mod tests {
         }
         {
             let elem = i.next().unwrap();
-            let expe = LogItem::State(Ok(String::from("finished successfully")));
+            let expe = LogItem::State(Ok(()));
             assert_eq!(*elem, expe, "Expected {}: {:?}", prettify_item(&expe), prettify_item(elem));
         }
         {

commit 5225a0130e80793ac6c52c30c82a79c21e4d871a

Fix: Add missing trailing quote
Fixes: 1d038bae2b ("Remove feature: State message in "OK" case.")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/script.rs
@@ -199,7 +199,7 @@ impl HelperDef for StateHelper {
             .ok_or_else(|| RenderError::new("Required parameter must be a string: state"))
             .and_then(|state| match state {
                 "OK" => {
-                    out.write("echo '#BUTIDO:STATE:OK\n")?;
+                    out.write("echo '#BUTIDO:STATE:OK'\n")?;
                     Ok(())
                 },
                 "ERR" => {