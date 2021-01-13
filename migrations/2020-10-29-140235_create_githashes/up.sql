cbb1457d62e60e6860320951e08b24b2c1b4d4c8
Matthias Beyer <mail@beyermatthias.de>
2021-01-13 08:31:24 +0100

    Add LICENSE file and license headers
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- migrations/2020-10-29-140235_create_githashes/up.sql
+++ migrations/2020-10-29-140235_create_githashes/up.sql
@@ -1,3 +1,13 @@
+--
+-- Copyright (c) 2020-2021 science+computing ag and other contributors
+--
+-- This program and the accompanying materials are made
+-- available under the terms of the Eclipse Public License 2.0
+-- which is available at https://www.eclipse.org/legal/epl-2.0/
+--
+-- SPDX-License-Identifier: EPL-2.0
+--
+
 -- Your SQL goes here
 CREATE TABLE githashes (
     id SERIAL PRIMARY KEY NOT NULL,
--- migrations/2020-10-29-142339_create_artifacts/down.sql
+++ migrations/2020-10-29-142339_create_artifacts/down.sql
@@ -1,2 +1,12 @@
+--
+-- Copyright (c) 2020-2021 science+computing ag and other contributors
+--
+-- This program and the accompanying materials are made
+-- available under the terms of the Eclipse Public License 2.0
+-- which is available at https://www.eclipse.org/legal/epl-2.0/
+--
+-- SPDX-License-Identifier: EPL-2.0
+--
+
 -- This file should undo anything in `up.sql`
 DROP TABLE artifacts
