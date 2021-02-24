014f1889bc8c8c658923148c1471ff3c35ff57e1
Matthias Beyer <matthias.beyer@atos.net>
2021-02-24 20:24:50 +0100

    Remove unused stuff from development shell
    Signed-off-by: Matthias Beyer <matthias.beyer@atos.net>

--- shell.nix
+++ shell.nix
@@ -7,38 +7,8 @@ let
 
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
 
-  pgcli-dev = pkgs.writeShellScriptBin "pgcli-dev" ''
-    exec ${pkgs.pgcli}/bin/pgcli -h localhost -p 5432 ${PG_DB} ${PG_USER}
-  '';
-
-  PG_USER           = "pgdev";
-  PG_DB             = "butido";
-  PG_PW             = "password";
-  PG_CONTAINER_NAME = "pg-dev-container";
-
-
-  example_1_env = {
-    BUTIDO_RELEASES     = "/tmp/example-1-releases";
-    BUTIDO_STAGING      = "/tmp/example-1-staging";
-    BUTIDO_SOURCE_CACHE = "/tmp/example-1-sources";
-    BUTIDO_LOG_DIR      = "/tmp/example-1-logs";
-    BUTIDO_REPO         = "/tmp/example-1-repo";
-
-    BUTIDO_DATABASE_HOST     = "localhost";
-    BUTIDO_DATABASE_PORT     = 5432;
-    BUTIDO_DATABASE_USER     = PG_USER;
-    BUTIDO_DATABASE_PASSWORD = PG_PW;
-    BUTIDO_DATABASE_NAME     = PG_DB;
-  };
-
-  selectedEnv = {
-    "1" = example_1_env;
-  }."${example}";
-
-
 in
-pkgs.mkShell (
-{
+pkgs.mkShell {
   buildInputs = with pkgs; [
     rustChannels.stable.rust-std
     rustChannels.stable.rust
@@ -47,7 +17,6 @@ pkgs.mkShell (
 
     diesel-cli
     pgcli
-    pgcli-dev
     postgresql
 
     cmake
@@ -57,12 +26,8 @@ pkgs.mkShell (
     pkgconfig
     which
     zlib
-
-    devd # development web-server for serving sources locally.
   ];
 
   LIBCLANG_PATH   = "${pkgs.llvmPackages.libclang}/lib";
-  inherit PG_USER PG_DB PG_PW PG_CONTAINER_NAME;
-} // selectedEnv
-)
+}
 
