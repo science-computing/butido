science-computing/butido

commit b3a6458ce34e3065192208826b2fc85edd4761f9

Add color support in LogItem::display() impl
Signed-off-by: Matthias Beyer <matthias.beyer@atos.net>

src/log/item.rs
@@ -9,6 +9,7 @@
 //
 
 use anyhow::Result;
+use colored::Colorize;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum LogItem {
@@ -29,15 +30,15 @@ pub enum LogItem {
 impl LogItem {
     pub fn display(&self) -> Result<Display> {
         match self {
-            LogItem::Line(s) => Ok(Display(String::from_utf8(s.to_vec())?)),
-            LogItem::Progress(u) => Ok(Display(format!("#BUTIDO:PROGRESS:{}", u))),
-            LogItem::CurrentPhase(p) => Ok(Display(format!("#BUTIDO:PHASE:{}", p))),
-            LogItem::State(Ok(())) => Ok(Display("#BUTIDO:STATE:OK".to_string())),
-            LogItem::State(Err(s)) => Ok(Display(format!("#BUTIDO:STATE:ERR:{}", s))),
+            LogItem::Line(s) => Ok(Display(String::from_utf8(s.to_vec())?.normal())),
+            LogItem::Progress(u) => Ok(Display(format!("#BUTIDO:PROGRESS:{}", u).cyan())),
+            LogItem::CurrentPhase(p) => Ok(Display(format!("#BUTIDO:PHASE:{}", p).cyan())),
+            LogItem::State(Ok(())) => Ok(Display("#BUTIDO:STATE:OK".to_string().green())),
+            LogItem::State(Err(s)) => Ok(Display(format!("#BUTIDO:STATE:ERR:{}", s).red())),
         }
     }
 }
 
 #[derive(parse_display::Display)]
 #[display("{0}")]
-pub struct Display(String);
+pub struct Display(colored::ColoredString);

commit 55715ffb14cd24a27fd70c0ad4154eedc6251976

Fix: Insert shouldn't be done with LogItem::display()
This fixes a nasty bug; because we used the LogItem::display() function to build
the log string that was put into the database, the database contained
_colorized_ log lines. Because of this, our parsing of the log text later (when
reading historical data from the database) failed to parse whether the job was
successfull or not.

The issue was actually introduced in b3a6458ce34e3065192208826b2fc85edd4761f9,
where we changed the `LogItem::display()` implementation.

With this fix in place, the log text in the database is corrected again (only
for new logs that get put into the database).

Fixes: b3a6458ce34e3065192208826b2fc85edd4761f9 ("Add color support in LogItem::display() impl")
Signed-off-by: Matthias Beyer <matthias.beyer@atos.net>
Tested-by: Matthias Beyer <matthias.beyer@atos.net>

src/endpoint/scheduler.rs
@@ -426,8 +426,7 @@ impl<'a> LogReceiver<'a> {
 
         Ok({
             accu.iter()
-                .map(crate::log::LogItem::display)
-                .map_ok(|d| d.to_string())
+                .map(crate::log::LogItem::raw)
                 .collect::<Result<Vec<String>>>()?
                 .join("\n")
         })

src/log/item.rs
@@ -8,6 +8,7 @@
 // SPDX-License-Identifier: EPL-2.0
 //
 
+use anyhow::Error;
 use anyhow::Result;
 use colored::Colorize;
 
@@ -35,6 +36,16 @@ impl LogItem {
             LogItem::CurrentPhase(p) => Ok(Display(format!("#BUTIDO:PHASE:{}", p).cyan())),
             LogItem::State(Ok(())) => Ok(Display("#BUTIDO:STATE:OK".to_string().green())),
             LogItem::State(Err(s)) => Ok(Display(format!("#BUTIDO:STATE:ERR:{}", s).red())),
+        }
+    }
+
+    pub fn raw(&self) -> Result<String> {
+        match self {
+            LogItem::Line(s) => String::from_utf8(s.to_vec()).map_err(Error::from),
+            LogItem::Progress(u) => Ok(format!("#BUTIDO:PROGRESS:{}", u)),
+            LogItem::CurrentPhase(p) => Ok(format!("#BUTIDO:PHASE:{}", p)),
+            LogItem::State(Ok(())) => Ok("#BUTIDO:STATE:OK".to_string()),
+            LogItem::State(Err(s)) => Ok(format!("#BUTIDO:STATE:ERR:{}", s)),
         }
     }
 }