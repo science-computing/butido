science-computing/butido

commit f20bf09292739e1bdbba9c1f8235a35f7d2d7712

Make progress bar for loading DAG optional
Signed-off-by: Matthias Beyer <matthias.beyer@atos.net>

src/package/dag.rs
@@ -40,14 +40,14 @@ impl Dag {
     pub fn for_root_package(
         p: Package,
         repo: &Repository,
-        progress: &ProgressBar,
+        progress: Option<&ProgressBar>,
     ) -> Result<Self> {
         fn add_sub_packages<'a>(
             repo: &'a Repository,
             mappings: &mut HashMap<&'a Package, daggy::NodeIndex>,
             dag: &mut daggy::Dag<&'a Package, i8>,
             p: &'a Package,
-            progress: &ProgressBar
+            progress: Option<&ProgressBar>
         ) -> Result<()> {
             p.get_self_packaged_dependencies()
                 .and_then_ok(|(name, constr)| {
@@ -63,7 +63,7 @@ impl Dag {
                         // recurse
                         packs.into_iter()
                             .try_for_each(|p| {
-                                progress.tick();
+                                progress.as_ref().map(|p| p.tick());
 
                                 let idx = dag.add_node(p);
                                 mappings.insert(p, idx);

src/commands/build.rs
@@ -226,7 +226,7 @@ pub async fn build(
 
     let dag = {
         let bar_tree_building = progressbars.bar();
-        let dag = Dag::for_root_package(package.clone(), &repo, &bar_tree_building)?;
+        let dag = Dag::for_root_package(package.clone(), &repo, Some(&bar_tree_building))?;
         bar_tree_building.finish_with_message("Finished loading Dag");
         dag
     };

src/commands/tree_of.rs
@@ -48,7 +48,7 @@ pub async fn tree_of(
         })
         .map(|package| {
             let bar_tree_building = progressbars.bar();
-            let tree = Dag::for_root_package(package.clone(), &repo, &bar_tree_building)?;
+            let tree = Dag::for_root_package(package.clone(), &repo, Some(&bar_tree_building))?;
             bar_tree_building.finish_with_message("Finished loading Tree");
             Ok(tree)
         })

commit cf2e7f74773d27a1db8e21cdca677d49acab0905

Fix: Pass Some(_) progressbar in tests
Fixes: f20bf09292739e1bdbba9c1f8235a35f7d2d7712 ("Make progress bar for loading DAG optional")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/package/dag.rs
@@ -182,7 +182,7 @@ mod tests {
         let repo = Repository::from(btree);
         let progress = ProgressBar::hidden();
 
-        let r = Dag::for_root_package(p1, &repo, &progress);
+        let r = Dag::for_root_package(p1, &repo, Some(&progress));
         assert!(r.is_ok());
     }
 
@@ -214,7 +214,7 @@ mod tests {
         let repo = Repository::from(btree);
         let progress = ProgressBar::hidden();
 
-        let dag = Dag::for_root_package(p1, &repo, &progress);
+        let dag = Dag::for_root_package(p1, &repo, Some(&progress));
         assert!(dag.is_ok());
         let dag = dag.unwrap();
         let ps = dag.all_packages();
@@ -303,7 +303,7 @@ mod tests {
         let repo = Repository::from(btree);
         let progress = ProgressBar::hidden();
 
-        let r = Dag::for_root_package(p1, &repo, &progress);
+        let r = Dag::for_root_package(p1, &repo, Some(&progress));
         assert!(r.is_ok());
         let r = r.unwrap();
         let ps = r.all_packages();
@@ -446,7 +446,7 @@ mod tests {
         let repo = Repository::from(btree);
         let progress = ProgressBar::hidden();
 
-        let r = Dag::for_root_package(p1, &repo, &progress);
+        let r = Dag::for_root_package(p1, &repo, Some(&progress));
         assert!(r.is_ok());
         let r = r.unwrap();
         let ps = r.all_packages();
@@ -551,7 +551,7 @@ mod tests {
         let repo = Repository::from(btree);
         let progress = ProgressBar::hidden();
 
-        let r = Dag::for_root_package(p1, &repo, &progress);
+        let r = Dag::for_root_package(p1, &repo, Some(&progress));
         assert!(r.is_ok());
         let r = r.unwrap();
         let ps = r.all_packages();