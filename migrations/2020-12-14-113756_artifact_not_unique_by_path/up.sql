de345521d9fbbb5ae641c27667b9ee7823fa8066
Matthias Beyer <matthias.beyer@atos.net>
2021-11-02 12:18:36 +0100

    Update Copyright string to 2020-2022
    Signed-off-by: Matthias Beyer <matthias.beyer@atos.net>

--- migrations/2020-12-14-113756_artifact_not_unique_by_path/up.sql
+++ migrations/2020-12-14-113756_artifact_not_unique_by_path/up.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2020-2021 science+computing ag and other contributors
+-- Copyright (c) 2020-2022 science+computing ag and other contributors
 --
 -- This program and the accompanying materials are made
 -- available under the terms of the Eclipse Public License 2.0
