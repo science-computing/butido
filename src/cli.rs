science-computing/butido

commit a53f3bfd0622f1ee955af053c52f1d1577695c90

Rewrite value fetching
Between clap 3.0 and clap 4.0, the way how values are fetched from the
CLI has changed. Notably the Arg::value_of and Arg::values_of functions
were replaced by something that can actually be a bit more typed.

This patch rewrites all these uses and introduces a bit more typing in
the CLI definition itself.

Types like integers or uuid::Uuid are moved to the CLI definition,
because these are trivial to make into clap ValueParser types.

More types should and will be transformed to be able to catch errors
already in CLI parsing instead of down the road during the execution of
butido.

Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/cli.rs
@@ -16,7 +16,7 @@ use clap::crate_version;
 use clap::Command;
 use clap::Arg;
 use clap::ArgGroup;
-use clap::builder::PossibleValueParser;
+use clap::builder::PossibleValuesParser;
 
 // Helper types to ship around stringly typed clap API.
 pub const IDENT_DEPENDENCY_TYPE_BUILD: &str = "build";
@@ -112,7 +112,7 @@ pub fn cli<'a>() -> Command {
             .version(crate_version!())
             .about("Generate and print commandline completions")
             .arg(Arg::new("shell")
-                .value_parser(PossibleValueParser::new(["bash", "elvish", "fish", "zsh"]))
+                .value_parser(PossibleValuesParser::new(["bash", "elvish", "fish", "zsh"]))
                 .default_value("bash")
                 .required(true)
                 .num_args(1)
@@ -140,7 +140,7 @@ pub fn cli<'a>() -> Command {
                     .num_args(1)
                     .long("tool")
                     .value_name("TOOL")
-                    .value_parser(PossibleValueParser::new(["psql", "pgcli"]))
+                    .value_parser(PossibleValuesParser::new(["psql", "pgcli"]))
                     .help("Use a specific tool")
                 )
             )
@@ -168,6 +168,7 @@ pub fn cli<'a>() -> Command {
                     .short('J')
                     .num_args(1)
                     .value_name("JOB UUID")
+                    .value_parser(clap::value_parser!(uuid::Uuid))
                     .help("Print only artifacts for a certain job")
                 )
             )
@@ -202,6 +203,7 @@ pub fn cli<'a>() -> Command {
                     .num_args(1)
                     .index(1)
                     .value_name("SUBMIT")
+                    .value_parser(clap::value_parser!(uuid::Uuid))
                     .help("The Submit to show details about")
                 )
             )
@@ -236,6 +238,7 @@ pub fn cli<'a>() -> Command {
                     .long("limit")
                     .num_args(1)
                     .value_name("LIMIT")
+                    .value_parser(clap::value_parser!(i64))
                     .help("Only list LIMIT submits")
                 )
                 .arg(Arg::new("for-commit")
@@ -270,6 +273,7 @@ pub fn cli<'a>() -> Command {
                     .short('S')
                     .num_args(1)
                     .value_name("UUID")
+                    .value_parser(clap::value_parser!(uuid::Uuid))
                     .help("Only list jobs of a certain submit")
                 )
 
@@ -288,6 +292,7 @@ pub fn cli<'a>() -> Command {
                     .short('L')
                     .num_args(1)
                     .value_name("LIMIT")
+                    .value_parser(clap::value_parser!(usize))
                     .help("Only list newest LIMIT jobs instead of all")
                 )
 
@@ -446,6 +451,7 @@ pub fn cli<'a>() -> Command {
                 .long("staging-dir")
                 .num_args(1)
                 .value_name("PATH")
+                .value_parser(clap::value_parser!(PathBuf))
                 .validator(dir_exists_validator)
                 .help("Do not throw dice on staging directory name, but hardcode for this run.")
             )
@@ -510,7 +516,7 @@ pub fn cli<'a>() -> Command {
                 .short('t')
                 .long("type")
                 .value_name("DEPENDENCY_TYPE")
-                .value_parser(PossibleValueParser::new([
+                .value_parser(PossibleValuesParser::new([
                     IDENT_DEPENDENCY_TYPE_BUILD,
                     IDENT_DEPENDENCY_TYPE_RUNTIME,
                 ]))
@@ -545,7 +551,7 @@ pub fn cli<'a>() -> Command {
                 .short('t')
                 .long("type")
                 .value_name("DEPENDENCY_TYPE")
-                .value_parser(PossibleValueParser::new([
+                .value_parser(PossibleValuesParser::new([
                     IDENT_DEPENDENCY_TYPE_BUILD,
                     IDENT_DEPENDENCY_TYPE_RUNTIME,
                 ]))
@@ -695,7 +701,7 @@ pub fn cli<'a>() -> Command {
                 .num_args(1..)
                 .long("dependency-type")
                 .value_name("DEPENDENCY_TYPE")
-                .value_parser(PossibleValueParser::new([
+                .value_parser(PossibleValuesParser::new([
                     IDENT_DEPENDENCY_TYPE_BUILD,
                     IDENT_DEPENDENCY_TYPE_RUNTIME,
                 ]))
@@ -863,6 +869,7 @@ pub fn cli<'a>() -> Command {
                     .num_args(1)
                     .long("timeout")
                     .value_name("TIMEOUT")
+                    .value_parser(clap::value_parser!(u64))
                     .help("Set timeout for download in seconds")
                 )
             )
@@ -933,6 +940,7 @@ pub fn cli<'a>() -> Command {
                     .num_args(1)
                     .index(1)
                     .value_name("SUBMIT")
+                    .value_parser(clap::value_parser!(uuid::Uuid))
                     .help("The submit uuid from which to release a package")
                 )
                 .arg(Arg::new("release_store_name")
@@ -1087,6 +1095,7 @@ pub fn cli<'a>() -> Command {
                     .long("times")
                     .short('n')
                     .value_name("N")
+                    .value_parser(clap::value_parser!(u64))
                     .default_value("10")
                     .help("How often to ping")
                 )
@@ -1095,6 +1104,7 @@ pub fn cli<'a>() -> Command {
                     .num_args(1)
                     .long("sleep")
                     .value_name("N")
+                    .value_parser(clap::value_parser!(u64))
                     .default_value("1")
                     .help("How long to sleep between pings")
                 )

src/commands/build.rs
@@ -72,14 +72,14 @@ pub async fn build(
 
     let shebang = Shebang::from({
         matches
-            .value_of("shebang")
-            .map(String::from)
+            .get_one::<String>("shebang")
+            .map(String::clone)
             .unwrap_or_else(|| config.shebang().clone())
     });
 
     let image_name = matches
-        .value_of("image")
-        .map(String::from)
+        .get_one::<String>("image")
+        .map(String::clone)
         .map(ImageName::from)
         .unwrap(); // safe by clap
     if config.docker().verify_images_present()
@@ -126,20 +126,21 @@ pub async fn build(
     info!("Endpoint config build");
 
     let pname = matches
-        .value_of("package_name")
-        .map(String::from)
+        .get_one::<String>("package_name")
+        .map(String::clone)
         .map(PackageName::from)
         .unwrap(); // safe by clap
 
     let pvers = matches
-        .value_of("package_version")
-        .map(String::from)
+        .get_one::<String>("package_version")
+        .map(String::clone)
         .map(PackageVersion::from);
     info!("We want {} ({:?})", pname, pvers);
 
     let additional_env = matches
-        .values_of("env")
+        .get_many::<String>("env")
         .unwrap_or_default()
+        .map(AsRef::as_ref)
         .map(crate::util::env::parse_to_env)
         .collect::<Result<Vec<(EnvironmentVariableName, String)>>>()?;
 
@@ -185,7 +186,7 @@ pub async fn build(
     let (staging_store, staging_dir, submit_id) = {
         let bar_staging_loading = progressbars.bar();
 
-        let (submit_id, p) = if let Some(staging_dir) = matches.value_of("staging_dir").map(PathBuf::from) {
+        let (submit_id, p) = if let Some(staging_dir) = matches.get_one::<PathBuf>("staging_dir") {
             info!(
                 "Setting staging dir to {} for this run",
                 staging_dir.display()
@@ -201,7 +202,7 @@ pub async fn build(
                 .context("Parsing directory name as UUID")
                 .with_context(|| anyhow!("Seems not to be a submit UUID: {}", uuid))?;
 
-            (uuid, staging_dir)
+            (uuid, staging_dir.to_path_buf())
         } else {
             let submit_id = uuid::Uuid::new_v4();
             let staging_dir = config

src/commands/db.rs
@@ -15,10 +15,10 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::str::FromStr;
 
+use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Error;
 use anyhow::Result;
-use anyhow::anyhow;
 use clap::ArgMatches;
 use colored::Colorize;
 use diesel::BelongingToDsl;
@@ -132,12 +132,13 @@ fn cli(db_connection_config: DbConnectionConfig<'_>, matches: &ArgMatches) -> Re
     }
 
     matches
-        .value_of("tool")
+        .get_one::<String>("tool")
+        .map(String::clone)
         .map(|s| vec![s])
-        .unwrap_or_else(|| vec!["psql", "pgcli"])
+        .unwrap_or_else(|| vec!["psql".to_string(), "pgcli".to_string()])
         .into_iter()
         .filter_map(|s| which::which(&s).ok().map(|path| (path, s)))
-        .map(|(path, s)| match s {
+        .map(|(path, s)| match s.as_ref() {
             "psql" => Ok(Box::new(Psql(path)) as Box<dyn PgCliCommand>),
             "pgcli" => Ok(Box::new(PgCli(path)) as Box<dyn PgCliCommand>),
             prog => Err(anyhow!("Unsupported pg CLI program: {}", prog)),
@@ -161,9 +162,7 @@ fn artifacts(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<(
     let hdrs = crate::commands::util::mk_header(vec!["Path", "Released", "Job"]);
     let conn = conn_cfg.establish_connection()?;
     let data = matches
-        .value_of("job_uuid")
-        .map(uuid::Uuid::parse_str)
-        .transpose()?
+        .get_one::<uuid::Uuid>("job_uuid")
         .map(|job_uuid| -> Result<_> {
             dsl::artifacts
                 .inner_join(schema::jobs::table)
@@ -185,11 +184,7 @@ fn artifacts(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<(
             let rel = rel
                 .map(|r| r.release_date.to_string())
                 .unwrap_or_else(|| String::from("no"));
-            vec![
-                artifact.path,
-                rel,
-                job.uuid.to_string(),
-            ]
+            vec![artifact.path, rel, job.uuid.to_string()]
         })
         .collect::<Vec<_>>();
 
@@ -249,11 +244,7 @@ fn images(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> 
 /// Implementation of the "db submit" subcommand
 fn submit(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
     let conn = conn_cfg.establish_connection()?;
-    let submit_id = matches.value_of("submit")
-        .map(uuid::Uuid::from_str)
-        .transpose()
-        .context("Parsing submit UUID")?
-        .unwrap(); // safe by clap
+    let submit_id = matches.get_one::<uuid::Uuid>("submit").unwrap(); // safe by clap
 
     let submit = models::Submit::with_id(&conn, &submit_id)
         .with_context(|| anyhow!("Loading submit '{}' from DB", submit_id))?;
@@ -288,7 +279,9 @@ fn submit(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> 
     let out = std::io::stdout();
     let mut outlock = out.lock();
 
-    indoc::writedoc!(outlock, r#"
+    indoc::writedoc!(
+        outlock,
+        r#"
             Submit   {submit_id}
             Date:    {submit_dt}
             Commit:  {submit_commit}
@@ -307,8 +300,20 @@ fn submit(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> 
         n_jobs_err = jobs_err.to_string().red(),
     )?;
 
-    let header = crate::commands::util::mk_header(["Job", "Success", "Package", "Version", "Container", "Endpoint", "Image"].to_vec());
-    let data = jobs.iter()
+    let header = crate::commands::util::mk_header(
+        [
+            "Job",
+            "Success",
+            "Package",
+            "Version",
+            "Container",
+            "Endpoint",
+            "Image",
+        ]
+        .to_vec(),
+    );
+    let data = jobs
+        .iter()
         .map(|job| {
             let image = models::Image::fetch_for_job(&conn, job)?
                 .ok_or_else(|| anyhow!("Image for job {} not found", job.uuid))?;
@@ -338,14 +343,21 @@ fn submit(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> 
 /// Implementation of the "db submits" subcommand
 fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
     let csv = matches.is_present("csv");
-    let limit = matches.value_of("limit").map(i64::from_str).transpose()?;
-    let hdrs = crate::commands::util::mk_header(vec!["Time", "UUID", "For Package", "For Package Version"]);
+    let limit = matches.get_one::<i64>("limit");
+    let hdrs = crate::commands::util::mk_header(vec![
+        "Time",
+        "UUID",
+        "For Package",
+        "For Package Version",
+    ]);
     let conn = conn_cfg.establish_connection()?;
-    let commit = matches.value_of("for-commit");
+    let commit = matches.get_one::<String>("for-commit");
 
     let query = schema::submits::table
         .order_by(schema::submits::id.desc()) // required for the --limit implementation
-        .inner_join(schema::githashes::table.on(schema::submits::repo_hash_id.eq(schema::githashes::id)))
+        .inner_join(
+            schema::githashes::table.on(schema::submits::repo_hash_id.eq(schema::githashes::id)),
+        )
         .inner_join(schema::images::table)
         .into_boxed();
 
@@ -355,13 +367,13 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
         query
     };
 
-    let query = if let Some(image) = matches.value_of("image") {
+    let query = if let Some(image) = matches.get_one::<String>("image") {
         query.filter(schema::images::name.eq(image))
     } else {
         query
     };
 
-    let submits = if let Some(pkgname) = matches.value_of("with_pkg").map(String::from) {
+    let submits = if let Some(pkgname) = matches.get_one::<String>("with_pkg") {
         // In the case of a with_pkg command, we must execute two queries on the database, as the
         // diesel framework does not yet support aliases for queries (see
         // https://github.com/diesel-rs/diesel/pull/2254).
@@ -372,11 +384,13 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
         // Get all submits which included the package, but were not necessarily made _for_ the package
         let query = query
             .inner_join(schema::jobs::table)
-            .inner_join(schema::packages::table.on(schema::jobs::package_id.eq(schema::packages::id)))
+            .inner_join(
+                schema::packages::table.on(schema::jobs::package_id.eq(schema::packages::id)),
+            )
             .filter(schema::packages::name.eq(&pkgname));
 
         let query = if let Some(limit) = limit {
-            query.limit(limit)
+            query.limit(*limit)
         } else {
             query
         };
@@ -387,21 +401,23 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
         schema::submits::table
             .order_by(schema::submits::id.desc()) // required for the --limit implementation
             .inner_join({
-                schema::packages::table.on(schema::submits::requested_package_id.eq(schema::packages::id))
+                schema::packages::table
+                    .on(schema::submits::requested_package_id.eq(schema::packages::id))
             })
             .filter(schema::submits::id.eq_any(submit_ids))
             .select((schema::submits::all_columns, schema::packages::all_columns))
             .load::<(models::Submit, models::Package)>(&conn)?
-    } else if let Some(pkgname) = matches.value_of("for_pkg") {
+    } else if let Some(pkgname) = matches.get_one::<String>("for_pkg") {
         // Get all submits _for_ the package
         let query = query
             .inner_join({
-                schema::packages::table.on(schema::submits::requested_package_id.eq(schema::packages::id))
+                schema::packages::table
+                    .on(schema::submits::requested_package_id.eq(schema::packages::id))
             })
             .filter(schema::packages::dsl::name.eq(&pkgname));
 
         if let Some(limit) = limit {
-            query.limit(limit)
+            query.limit(*limit)
         } else {
             query
         }
@@ -410,14 +426,17 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
     } else if let Some(limit) = limit {
         query
             .inner_join({
-                schema::packages::table.on(schema::submits::requested_package_id.eq(schema::packages::id))
+                schema::packages::table
+                    .on(schema::submits::requested_package_id.eq(schema::packages::id))
             })
             .select((schema::submits::all_columns, schema::packages::all_columns))
-            .limit(limit)
+            .limit(*limit)
             .load::<(models::Submit, models::Package)>(&conn)?
     } else {
-        query.inner_join({
-                schema::packages::table.on(schema::submits::requested_package_id.eq(schema::packages::id))
+        query
+            .inner_join({
+                schema::packages::table
+                    .on(schema::submits::requested_package_id.eq(schema::packages::id))
             })
             .select((schema::submits::all_columns, schema::packages::all_columns))
             .load::<(models::Submit, models::Package)>(&conn)?
@@ -433,7 +452,11 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
         ]
     };
 
-    let data = submits.into_iter().rev().map(submit_to_vec).collect::<Vec<_>>();
+    let data = submits
+        .into_iter()
+        .rev()
+        .map(submit_to_vec)
+        .collect::<Vec<_>>();
 
     if data.is_empty() {
         info!("No submits in database");
@@ -448,13 +471,7 @@ fn submits(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()>
 fn jobs(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
     let csv = matches.is_present("csv");
     let hdrs = crate::commands::util::mk_header(vec![
-        "Submit",
-        "Job",
-        "Time",
-        "Endpoint",
-        "Success",
-        "Package",
-        "Version",
+        "Submit", "Job", "Time", "Endpoint", "Success", "Package", "Version",
     ]);
     let conn = conn_cfg.establish_connection()?;
     let older_than_filter = get_date_filter("older_than", matches)?;
@@ -466,7 +483,7 @@ fn jobs(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
         .inner_join(schema::packages::table)
         .into_boxed();
 
-    if let Some(submit_uuid) = matches.value_of("submit_uuid").map(uuid::Uuid::parse_str).transpose()? {
+    if let Some(submit_uuid) = matches.get_one::<uuid::Uuid>("submit_uuid") {
         sel = sel.filter(schema::submits::uuid.eq(submit_uuid))
     }
 
@@ -474,19 +491,28 @@ fn jobs(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
     //
     // If we get a filter for environment on CLI, we fetch all job ids that are associated with the
     // passed environment variables and make `sel` filter for those.
-    if let Some((name, val)) = matches.value_of("env_filter").map(crate::util::env::parse_to_env).transpose()? {
+    if let Some((name, val)) = matches
+        .get_one::<String>("env_filter")
+        .map(AsRef::as_ref)
+        .map(crate::util::env::parse_to_env)
+        .transpose()?
+    {
         debug!("Filtering for ENV: {} = {}", name, val);
         let jids = schema::envvars::table
             .filter({
                 use crate::diesel::BoolExpressionMethods;
-                schema::envvars::dsl::name.eq(name.as_ref())
+                schema::envvars::dsl::name
+                    .eq(name.as_ref())
                     .and(schema::envvars::dsl::value.eq(val))
             })
             .inner_join(schema::job_envs::table)
             .select(schema::job_envs::job_id)
             .load::<i32>(&conn)?;
 
-        debug!("Filtering for these IDs (because of env filter): {:?}", jids);
+        debug!(
+            "Filtering for these IDs (because of env filter): {:?}",
+            jids
+        );
         sel = sel.filter(schema::jobs::dsl::id.eq_any(jids));
     }
 
@@ -498,21 +524,26 @@ fn jobs(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
         sel = sel.filter(schema::submits::dsl::submit_time.gt(datetime))
     }
 
-    if let Some(limit) = matches.value_of("limit").map(i64::from_str).transpose()? {
-        sel = sel.limit(limit)
+    if let Some(limit) = matches.get_one::<i64>("limit") {
+        sel = sel.limit(*limit)
     }
 
-    if let Some(ep_name) = matches.value_of("endpoint") {
+    if let Some(ep_name) = matches.get_one::<String>("endpoint") {
         sel = sel.filter(schema::endpoints::name.eq(ep_name))
     }
 
-    if let Some(pkg_name) = matches.value_of("package") {
+    if let Some(pkg_name) = matches.get_one::<String>("package") {
         sel = sel.filter(schema::packages::name.eq(pkg_name))
     }
 
     let data = sel
         .order_by(schema::jobs::id.desc()) // required for the --limit implementation
-        .load::<(models::Job, models::Submit, models::Endpoint, models::Package)>(&conn)?
+        .load::<(
+            models::Job,
+            models::Submit,
+            models::Endpoint,
+            models::Package,
+        )>(&conn)?
         .into_iter()
         .rev() // required for the --limit implementation
         .map(|(job, submit, ep, package)| {
@@ -543,7 +574,11 @@ fn jobs(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
 }
 
 /// Implementation of the "db job" subcommand
-fn job(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &ArgMatches) -> Result<()> {
+fn job(
+    conn_cfg: DbConnectionConfig<'_>,
+    config: &Configuration,
+    matches: &ArgMatches,
+) -> Result<()> {
     let script_highlight = !matches.is_present("no_script_highlight");
     let script_line_numbers = !matches.is_present("no_script_line_numbers");
     let configured_theme = config.script_highlight_theme();
@@ -551,11 +586,7 @@ fn job(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &ArgMa
     let show_script = matches.is_present("show_script");
     let csv = matches.is_present("csv");
     let conn = conn_cfg.establish_connection()?;
-    let job_uuid = matches
-        .value_of("job_uuid")
-        .map(uuid::Uuid::parse_str)
-        .transpose()?
-        .unwrap();
+    let job_uuid = matches.get_one::<uuid::Uuid>("job_uuid").unwrap();
 
     let data = schema::jobs::table
         .filter(schema::jobs::dsl::uuid.eq(job_uuid))
@@ -718,12 +749,8 @@ fn job(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &ArgMa
 
 /// Implementation of the subcommand "db log-of"
 fn log_of(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> {
-    let conn   = conn_cfg.establish_connection()?;
-    let job_uuid = matches
-        .value_of("job_uuid")
-        .map(uuid::Uuid::parse_str)
-        .transpose()?
-        .unwrap();
+    let conn = conn_cfg.establish_connection()?;
+    let job_uuid = matches.get_one::<uuid::Uuid>("job_uuid").unwrap();
     let out = std::io::stdout();
     let mut lock = out.lock();
 
@@ -734,23 +761,33 @@ fn log_of(conn_cfg: DbConnectionConfig<'_>, matches: &ArgMatches) -> Result<()> 
         .map_err(Error::from)
         .and_then(|s| crate::log::ParsedLog::from_str(&s))?
         .into_iter()
-        .map(|line| line.display().and_then(|d| writeln!(lock, "{}", d).map_err(Error::from)))
+        .map(|line| {
+            line.display()
+                .and_then(|d| writeln!(lock, "{}", d).map_err(Error::from))
+        })
         .collect::<Result<Vec<()>>>()
         .map(|_| ())
 }
 
 /// Implementation of the "db releases" subcommand
-fn releases(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &ArgMatches) -> Result<()> {
-    let csv    = matches.is_present("csv");
-    let conn   = conn_cfg.establish_connection()?;
+fn releases(
+    conn_cfg: DbConnectionConfig<'_>,
+    config: &Configuration,
+    matches: &ArgMatches,
+) -> Result<()> {
+    let csv = matches.is_present("csv");
+    let conn = conn_cfg.establish_connection()?;
     let header = crate::commands::util::mk_header(["Package", "Version", "Date", "Path"].to_vec());
     let mut query = schema::jobs::table
         .inner_join(schema::packages::table)
         .inner_join(schema::artifacts::table)
-        .inner_join(schema::releases::table
-            .on(schema::releases::artifact_id.eq(schema::artifacts::id)))
-        .inner_join(schema::release_stores::table
-            .on(schema::release_stores::id.eq(schema::releases::release_store_id)))
+        .inner_join(
+            schema::releases::table.on(schema::releases::artifact_id.eq(schema::artifacts::id)),
+        )
+        .inner_join(
+            schema::release_stores::table
+                .on(schema::release_stores::id.eq(schema::releases::release_store_id)),
+        )
         .order_by(schema::packages::dsl::name.asc())
         .then_order_by(schema::packages::dsl::version.asc())
         .then_order_by(schema::releases::release_date.asc())
@@ -764,11 +801,11 @@ fn releases(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &
         query = query.filter(schema::releases::release_date.gt(date));
     }
 
-    if let Some(store) = matches.value_of("store") {
+    if let Some(store) = matches.get_one::<String>("store") {
         query = query.filter(schema::release_stores::dsl::store_name.eq(store));
     }
 
-    if let Some(pkg) = matches.value_of("package") {
+    if let Some(pkg) = matches.get_one::<String>("package") {
         query = query.filter(schema::packages::dsl::name.eq(pkg));
     }
 
@@ -780,10 +817,18 @@ fn releases(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &
             let rst = schema::release_stores::all_columns;
             (art, pac, rel, rst)
         })
-        .load::<(models::Artifact, models::Package, models::Release, models::ReleaseStore)>(&conn)?
+        .load::<(
+            models::Artifact,
+            models::Package,
+            models::Release,
+            models::ReleaseStore,
+        )>(&conn)?
         .into_iter()
         .filter_map(|(art, pack, rel, rstore)| {
-            let p = config.releases_directory().join(rstore.store_name).join(&art.path);
+            let p = config
+                .releases_directory()
+                .join(rstore.store_name)
+                .join(&art.path);
 
             if p.is_file() {
                 Some(vec![
@@ -793,7 +838,12 @@ fn releases(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &
                     p.display().to_string(),
                 ])
             } else {
-                log::warn!("Released file for {} {} not found: {}", pack.name, pack.version, p.display());
+                log::warn!(
+                    "Released file for {} {} not found: {}",
+                    pack.name,
+                    pack.version,
+                    p.display()
+                );
                 None
             }
         })
@@ -808,4 +858,3 @@ fn releases(conn_cfg: DbConnectionConfig<'_>, config: &Configuration, matches: &
 fn is_job_successfull(job: &models::Job) -> Result<Option<bool>> {
     crate::log::ParsedLog::from_str(&job.log_text).map(|pl| pl.is_successfull().to_bool())
 }
-

src/commands/dependencies_of.rs
@@ -34,8 +34,8 @@ pub async fn dependencies_of(
 
     let package_filter = {
         let name = matches
-            .value_of("package_name")
-            .map(String::from)
+            .get_one::<String>("package_name")
+            .map(String::clone)
             .map(PackageName::from)
             .unwrap();
         trace!("Checking for package with name = {}", name);

commit 5ad7cce12a8af75447ce45311dc2ef9883116041

Fix: Number of args for --env in "build" subcommand
Fixes: a53f3bfd0622f1ee955af053c52f1d1577695c90 ("Rewrite value fetching")
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/cli.rs
@@ -465,7 +465,7 @@ pub fn cli() -> Command {
 
             .arg(Arg::new("env")
                 .required(false)
-                .num_args(0)
+                .num_args(1)
                 .short('E')
                 .long("env")
                 .value_parser(ValueParser::new(env_pass_validator))