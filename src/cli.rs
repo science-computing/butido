debbbc2a23142e09b1c9acad479e20fca718eecf
Matthias Beyer <mail@beyermatthias.de>
2022-11-18 16:43:19 +0100

    Revert "Rewrite dir_exists_validator to be a value parser"
    This reverts commit 7ca2a339758c0a651d4750ba8df4ac365f1ad2be.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/cli.rs
+++ src/cli.rs
@@ -17,7 +17,6 @@ use clap::Command;
 use clap::Arg;
 use clap::ArgGroup;
 use clap::builder::PossibleValuesParser;
-use clap::builder::ValueParser;
 
 // Helper types to ship around stringly typed clap API.
 pub const IDENT_DEPENDENCY_TYPE_BUILD: &str = "build";
@@ -452,7 +451,8 @@ pub fn cli<'a>() -> Command {
                 .long("staging-dir")
                 .num_args(1)
                 .value_name("PATH")
-                .value_parser(ValueParser::new(dir_exists_validator))
+                .value_parser(clap::value_parser!(PathBuf))
+                .validator(dir_exists_validator)
                 .help("Do not throw dice on staging directory name, but hardcode for this run.")
             )
 
@@ -623,7 +623,7 @@ pub fn cli<'a>() -> Command {
                 .num_args(1)
                 .long("staging-dir")
                 .value_name("PATH")
-                .value_parser(ValueParser::new(dir_exists_validator))
+                .validator(dir_exists_validator)
                 .help("Also consider this staging dir when searching for artifacts")
             )
             .arg(Arg::new("env_filter")
@@ -1358,10 +1358,9 @@ fn env_pass_validator(s: &str) -> Result<(), String> {
     }
 }
 
-fn dir_exists_validator(s: &str) -> Result<PathBuf, String> {
-    let pb = PathBuf::from(s);
-    if pb.is_dir() {
-        Ok(pb)
+fn dir_exists_validator(s: &str) -> Result<(), String> {
+    if PathBuf::from(&s).is_dir() {
+        Ok(())
     } else {
         Err(format!("Directory does not exist: {}", s))
     }
