science-computing/butido

commit f2567d045bc2cafaa7eb012bfdf424f38049f119

clap::builder: Replace Arg::validator with Arg::value_parser
The `validator` [0] method got replaced with the `value_parser` [1]
method: "replace `arg.validator(...)` with `arg.value_parser(...)`".

[0]: https://docs.rs/clap/3.2.23/clap/builder/struct.Arg.html#method.validator
[1]: https://docs.rs/clap/3.2.23/clap/builder/struct.Arg.html#method.value_parser

Signed-off-by: Michael Weiss <michael.weiss@atos.net>

src/cli.rs
@@ -458,7 +458,7 @@ pub fn cli<'a>() -> Command<'a> {
                 .long("staging-dir")
                 .takes_value(true)
                 .value_name("PATH")
-                .validator(dir_exists_validator)
+                .value_parser(dir_exists_validator)
                 .help("Do not throw dice on staging directory name, but hardcode for this run.")
             )
 
@@ -476,7 +476,7 @@ pub fn cli<'a>() -> Command<'a> {
                 .takes_value(true)
                 .short('E')
                 .long("env")
-                .validator(env_pass_validator)
+                .value_parser(env_pass_validator)
                 .help("Pass environment variable to all build jobs")
                 .long_help(indoc::indoc!(r#"
                     Pass these variables to each build job.
@@ -625,7 +625,7 @@ pub fn cli<'a>() -> Command<'a> {
                 .long("staging-dir")
                 .takes_value(true)
                 .value_name("PATH")
-                .validator(dir_exists_validator)
+                .value_parser(dir_exists_validator)
                 .help("Also consider this staging dir when searching for artifacts")
             )
             .arg(Arg::new("env_filter")
@@ -636,7 +636,7 @@ pub fn cli<'a>() -> Command<'a> {
                 .short('E')
                 .takes_value(true)
                 .value_name("KV")
-                .validator(env_pass_validator)
+                .value_parser(env_pass_validator)
                 .help("Filter for this \"key=value\" environment variable")
             )
             .arg(Arg::new("image")
@@ -1041,7 +1041,7 @@ pub fn cli<'a>() -> Command<'a> {
                 .takes_value(true)
                 .short('E')
                 .long("env")
-                .validator(env_pass_validator)
+                .value_parser(env_pass_validator)
                 .help("Additional env to be passed when building packages")
                 .long_help(indoc::indoc!(r#"
                     Additional env to be passed when building packages.
@@ -1118,7 +1118,7 @@ pub fn cli<'a>() -> Command<'a> {
                         .takes_value(true)
                         .value_name("TIMEOUT")
                         .help("Timeout in seconds")
-                        .validator(parse_u64)
+                        .value_parser(parse_u64)
                     )
                 )
                 .subcommand(Command::new("list")
@@ -1167,7 +1167,7 @@ pub fn cli<'a>() -> Command<'a> {
                         .takes_value(true)
                         .value_name("LIMIT")
                         .help("Only list LIMIT processes for each container")
-                        .validator(parse_usize)
+                        .value_parser(parse_usize)
                     )
                 )
             )
@@ -1375,7 +1375,7 @@ fn arg_older_than_date(about: &str) -> Arg<'_> {
                 years, year, y -- defined as 365.25 days
 
         "#)
-        .validator(parse_date_from_string)
+        .value_parser(parse_date_from_string)
 }
 
 fn arg_newer_than_date(about: &str) -> Arg<'_> {
@@ -1404,7 +1404,7 @@ fn arg_newer_than_date(about: &str) -> Arg<'_> {
                 years, year, y -- defined as 365.25 days
 
         "#)
-        .validator(parse_date_from_string)
+        .value_parser(parse_date_from_string)
 }
 
 fn parse_date_from_string(s: &str) -> std::result::Result<(), String> {

commit cd0c30ed2f0ff2672b06b156e3dfeba79c2bfa57

clap: Fix all custom value parsers
This fixes a regression from f2567d0 that causes panics/crashes.

A `clap::builder::ValueParser` doesn't only parse/validate argument
values but also determines the final type of the argument and handles
the conversion. From the documentation [0]:
> `ValueParser` defines how to convert a raw argument value into a
> validated and typed value for use within an application.

A custom `TypedValueParser` [1] can be implemented via
`Fn(&str) -> Result<T, E>`. In this case, the type `T` represents the
type of the argument after parsing (as used by the application).

Our custom value parsers used the "unit" type `()` for the result.
This caused panics at runtime due to type mismatches that couldn't be
detected during compile time. An example from a debug build:
```
$ butido build -E DEBUG=1 -I local:default tree 1.8.0
[00:00:16] (100%): ████████████████████████████████████████ | Repository loading finished
2023-03-13T20:14:21.881237Z  INFO butido::commands::build: Endpoint config build
2023-03-13T20:14:21.881503Z  INFO butido::commands::build: We want tree (Some(PackageVersion("1.8.0")))
thread 'main' panicked at 'Mismatch between definition and access of `env`. Could not downcast to alloc::string::String, need to downcast to ()
', src/commands/build.rs:141:10
note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
```

It would be best to use the desired types instead of `String`s to avoid
code-duplication but I decided to only fix the regression for now and
improve the typing in a dedicated PR as that requires additional changes
and further improvements can be made (more typing, the parse_{usize,u64}
functions aren't required anymore, etc.).

[0]: https://docs.rs/clap/latest/clap/builder/struct.ValueParser.html
[1]: https://docs.rs/clap/latest/clap/builder/trait.TypedValueParser.html

Signed-off-by: Michael Weiss <michael.weiss@atos.net>

src/cli.rs
@@ -1315,7 +1315,7 @@ fn script_arg_no_highlight<'a>() -> clap::Arg<'a> {
 /// Naive check whether 's' is a 'key=value' pair or an existing environment variable
 ///
 /// TODO: Clean up this spaghetti code
-fn env_pass_validator(s: &str) -> Result<(), String> {
+fn env_pass_validator(s: &str) -> Result<String, String> {
     use crate::util::parser::*;
     let parser = {
         let key = (letters() + ((letters() | numbers() | under()).repeat(0..)))
@@ -1336,14 +1336,14 @@ fn env_pass_validator(s: &str) -> Result<(), String> {
         }
         Ok((k, v)) => {
             debug!("Env pass valiation: '{}={}'", k, v);
-            Ok(())
+            Ok(s.to_owned())
         }
     }
 }
 
-fn dir_exists_validator(s: &str) -> Result<(), String> {
+fn dir_exists_validator(s: &str) -> Result<String, String> {
     if PathBuf::from(&s).is_dir() {
-        Ok(())
+        Ok(s.to_owned())
     } else {
         Err(format!("Directory does not exist: {s}"))
     }
@@ -1407,7 +1407,7 @@ fn arg_newer_than_date(about: &str) -> Arg<'_> {
         .value_parser(parse_date_from_string)
 }
 
-fn parse_date_from_string(s: &str) -> std::result::Result<(), String> {
+fn parse_date_from_string(s: &str) -> std::result::Result<String, String> {
     humantime::parse_duration(s)
         .map_err(|e| e.to_string())
         .map(|_| ())
@@ -1422,14 +1422,15 @@ fn parse_date_from_string(s: &str) -> std::result::Result<(), String> {
                 .map_err(|e| e.to_string())
                 .map(|_| ())
         })
+        .map(|_| s.to_owned())
 }
 
-fn parse_usize(s: &str) -> std::result::Result<(), String> {
-    usize::from_str(s) .map_err(|e| e.to_string()).map(|_| ())
+fn parse_usize(s: &str) -> std::result::Result<String, String> {
+    usize::from_str(s) .map_err(|e| e.to_string()).map(|_| s.to_owned())
 }
 
-fn parse_u64(s: &str) -> std::result::Result<(), String> {
-    u64::from_str(s).map_err(|e| e.to_string()).map(|_| ())
+fn parse_u64(s: &str) -> std::result::Result<String, String> {
+    u64::from_str(s).map_err(|e| e.to_string()).map(|_| s.to_owned())
 }
 
 #[cfg(test)]