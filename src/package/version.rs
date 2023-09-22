8d12a41979bc1d6e2520a1ad45eb2f4bad53b398
Michael Weiss <michael.weiss@atos.net>
2023-09-22 21:01:52 +0200

    Improve the error message when parsing package dependency specifications
    The old error message lacked a lot of context so it was really hard to
    find the problematic specification in the package repo source code.
    This makes it quite a bit easier but it's still far from perfect
    (ideally, we'd emit the error while parsing the files in the packages
    repository so that we can output the file path and line number as well).
    
    Before this change:
    ```
    Error: tree-of command failed
    
    Caused by:
        0: A package version constraint must have a comparator and a version string, like so: =0.1.0
        1: Failed to parse package version constraint
        2: Mismatch at 0: expect: 61, found: 60
    ```
    
    With this change (one can `grep` for "libXYZ <1" in the packages repo):
    ```
    Error: tree-of command failed
    
    Caused by:
        0: Could not parse the following package dependency string: libXYZ <1
        1: A package version constraint must have a comparator (only `=` is currently supported) and a version string, like so: =0.1.0
        2: Failed to parse the following package version constraint: <1
        3: Mismatch at 0: expect: 61, found: 60
    ```
    (note: 61 is the decimal representation of `=` and 60 of `<`)
    
    Signed-off-by: Michael Weiss <michael.weiss@atos.net>

--- src/package/version.rs
+++ src/package/version.rs
@@ -10,6 +10,7 @@
 
 use std::ops::Deref;
 
+use anyhow::anyhow;
 use anyhow::Context;
 use anyhow::Error;
 use anyhow::Result;
@@ -64,8 +65,8 @@ impl std::convert::TryFrom<&str> for PackageVersionConstraint {
     fn try_from(s: &str) -> Result<Self> {
         PackageVersionConstraint::parser()
             .parse(s.as_bytes())
-            .context("Failed to parse package version constraint")
-            .context("A package version constraint must have a comparator and a version string, like so: =0.1.0")
+            .context(anyhow!("Failed to parse the following package version constraint: {}", s))
+            .context("A package version constraint must have a comparator (only `=` is currently supported) and a version string, like so: =0.1.0")
             .map_err(Error::from)
     }
 }
