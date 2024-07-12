science-computing/butido

commit 2f8034b54a54b38f933d4e0901376f7d24bddb0e

Trigger Source Verification After Source Download
This ensures that the 'source verify' process is triggered
immediately after the 'source download' is completed, enhancing the
workflow's integrity by verifying the source as soon as it is
downloaded.

Fixes #371

Signed-off-by: Nico Steinle <nico.steinle@eviden.com>

src/commands/source/download.rs
@@ -314,5 +314,8 @@ pub async fn download(
     }
 
     debug!("r = {:?}", r);
+
+    super::verify(matches, config, repo, progressbars).await?;
+
     r
 }

commit 2c0d17e0e239de44dc0a1df22153b87343b76be9

Fix the error propagation of the "source download" command
The additional source verification in 2f8034b introduced a regression by
returning the source verification errors before the errors of the
download operation (the download errors therefore get omitted and users
get misleading errors that the sources are missing and that the
verification did therefore fail).

This restores the proper error messages and avoids unnecessary source
verification by returning errors of the download operation before
triggering the verification.

We also don't need the debug output as we already return the errors and
I've put `Ok(())` at the end to make it a bit more readable.

Signed-off-by: Michael Weiss <michael.weiss@eviden.com>

src/commands/source/download.rs
@@ -20,7 +20,7 @@ use clap::ArgMatches;
 use tokio::io::AsyncWriteExt;
 use tokio::sync::Mutex;
 use tokio_stream::StreamExt;
-use tracing::{debug, info, trace, warn};
+use tracing::{info, trace, warn};
 
 use crate::config::*;
 use crate::package::PackageName;
@@ -305,13 +305,12 @@ pub async fn download(
 
     if r.is_err() {
         progressbar.lock().await.error().await;
+        return r;
     } else {
         progressbar.lock().await.success().await;
     }
 
-    debug!("r = {:?}", r);
-
     super::verify(matches, config, repo, progressbars).await?;
 
-    r
+    Ok(())
 }